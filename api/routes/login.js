baeef8d4f35d6abd7bd7eb02ea9b703278a1d5c7
Colin McNeil <colinmcneil@gmail.com>
2018-08-03 12:46:44 -0400

    Fix major secuirty vulnerability with netid session

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,6 +1,6 @@
 const { Router } = require("express")
 const Cas = require('cas-authentication');
-
+console.log(process.env.NODE_ENV)
 const CASOPTS = {
     cas_url: 'https://login.uconn.edu/cas',
     service_url: process.env.NODE_ENV === "development" ?
@@ -8,8 +8,8 @@ const CASOPTS = {
     cas_version: '2.0',
     renew: true,
     session_name: 'netid',
-    is_dev_mode: process.env.NODE_ENV === 'development',
-    dev_mode_user: process.env.CAS_DEV_USER,
+    //is_dev_mode: process.env.NODE_ENV === 'development',
+    //dev_mode_user: process.env.CAS_DEV_USER,
 }
 
 const cas = new Cas(CASOPTS)
@@ -19,10 +19,9 @@ const router = Router()
 router.get("/login", cas.bounce, (req, res) => {
     console.log(req.session)
     res.cookie('user', req.session.netid, { httpOnly: false })
-    res.redirect('/admin')
+    res.render('admin')
 })
 router.get('/logout', (req, res) => {
-    req.session = null
     res.redirect('/')
 })
 
@@ -30,15 +29,13 @@ router.use(async (req, res, next) => {
     // The user cookie is sent to the frontend so it knows who's currently logged
     // in. If the session is invalid, then we'll want to invalidate that cookie
     // // as well.
-    console.log(req.path)
-    if (req.path !== '/admin') return next();
-
-
     if (!req.session.netid) {
         res.cookie('user', null)
-        res.redirect('/login')
+
     }
-    next()
+    if (req.path !== '/admin') return next();
+    cas.bounce(req, res, next )
+    
     
 })
 
