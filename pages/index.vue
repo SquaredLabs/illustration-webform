f75f9930bddd2367d24935e83b215f5b09e57a5a
Colin McNeil <colinmcneil@gmail.com>
2018-08-06 13:41:28 -0400

    Changed spacing for better visual comprehension

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="formContainer">
+
     <div class = "alert" v-show="alertText!=''">
       <h3> Submitted: </h3>
       <p>{{alertText}}</p>
       <button class="copyContact alertButton" v-on:click="alertText=''">Okay</button>
     </div>
+
     <div class ="form branding">
       <p>UCONN</p>
       <p>Squared Labs</p>
@@ -22,14 +24,15 @@
 
       <span class="questionSub">NO *Paid Service</span>
       <checkBox id="NO" :model.sync="cover" :value="false" type="radio" name="serviceCover" />
-
     </div>
+
     <hr>
 
     <div class="form">
       <h2 class="question">Provide a brief description of what you would like illustrated:</h2>
       <textarea v-model="description" class="description textInput" placeholder="description"/>
     </div>
+
     <hr>
 
     <div class="form oneliner">
@@ -40,9 +43,8 @@
         <p>No</p>
         <checkBox :change="checkDate" :model.sync="deadline" :value="false" type="radio" name="deadline" />
       </div>
-      
-      
     </div>
+
     <hr>
 
     <div class="form">
@@ -64,8 +66,8 @@
       (for the illustrators to work within journal standards).
       </h2>
       <input v-model="journalName" type="text" class="textInput">
-      
     </div>
+
     <hr>
 
     <div class="form">
@@ -82,37 +84,34 @@
       <h2 class="question">Expected KFS account number note, 
       there will be no billing until a preliminary quote is provided):
       </h2>
-      <input v-model="KFS" type="text" class="textInput" placeholder="KFS">
-      
-      
+      <input v-model="KFS" type="text" class="textInput" placeholder="KFS"> 
     </div>
+
     <hr>
+
     <div class="form">
       <button :class="{disabled: submitText!=='Submit'}" class="button" @click="submit">{{ submitText }}</button>
     </div>
     
-
-
   </div>
 </template>
 
 <script>
+import Dropzone from "nuxt-dropzone";
+import "nuxt-dropzone/dropzone.css";
+import checkBox from "~/components/checkBox.vue";
+import datePicker from "vuejs-datepicker";
 
-import Dropzone from 'nuxt-dropzone'
-import 'nuxt-dropzone/dropzone.css'
-import checkBox from "~/components/checkBox.vue"
-import datePicker from "vuejs-datepicker"
+const verifyForm = require("~/lib/VerifyForm");
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
-const verifyForm = require("~/lib/VerifyForm")
-require("es6-promise").polyfill()
-require("isomorphic-fetch")
-
-function submitFile(dropzone, wo){
-  if(dropzone.getQueuedFiles().length===0) return
+function submitFile(dropzone, wo) {
+  if (dropzone.getQueuedFiles().length === 0) return;
   dropzone.on("sending", function(file, xhr, formData) {
     formData.append("wo", wo);
   });
-  dropzone.processQueue()
+  dropzone.processQueue();
 }
 
 export default {
@@ -132,69 +131,70 @@ export default {
     contactEmail: "",
     contactPhone: "",
     KFS: "",
-    alertText: "", 
+    alertText: "",
     uploadOptions: {
       url: "/api/uploadFile",
-      autoProcessQueue:false,
-      paramName:'file'
+      autoProcessQueue: false,
+      paramName: "file"
     }
   }),
+
   computed: {
     submitText() {
-      let errorText = verifyForm(this.$data)
-      return errorText || "Submit"
+      let errorText = verifyForm(this.$data);
+      return errorText || "Submit";
     }
   },
+
   methods: {
     checkDate(date) {
-      if (date !== null) return (this.deadline = true)
-      this.deadlineDate = ""
-      return (this.deadline = false)
+      if (date !== null) return (this.deadline = true);
+      this.deadlineDate = "";
+      return (this.deadline = false);
     },
     async submit() {
       let response = await fetch("/api/submit", {
         method: "post",
         headers: { "Content-Type": "application/json; charset=utf-8" },
         body: JSON.stringify(this.$data)
-      })
+      });
       if (response.status >= 400) {
-        console.error("Bad response from server")
+        console.error("Bad response from server");
       }
-      let jsonRes = await response.json()
-      this.alertText = jsonRes.text
+      let jsonRes = await response.json();
+      this.alertText = jsonRes.text;
       if (jsonRes.status === 1) {
-          let wo = jsonRes.wo
-          submitFile(this.$refs.article.dropzone, wo)
-          submitFile(this.$refs.reference.dropzone, wo)
-          submitFile(this.$refs.additional.dropzone, wo)
-          submitFile(this.$refs.original.dropzone, wo)
-          if(process.env.NODE_ENV=='development') return
-          this.cover = false
-          this.description = ""
-          this.deadline = false
-          this.deadlineDate = ""
-          this.journalName = ""
-          this.contactName = ""
-          this.contactEmail = ""
-          this.contactPhone = ""
-          this.KFS = ""
-          //article,reference,additional,original
-          
-
+        let wo = jsonRes.wo;
+        submitFile(this.$refs.article.dropzone, wo);
+        submitFile(this.$refs.reference.dropzone, wo);
+        submitFile(this.$refs.additional.dropzone, wo);
+        submitFile(this.$refs.original.dropzone, wo);
+        if (process.env.NODE_ENV == "development") return;
+        this.cover = false;
+        this.description = "";
+        this.deadline = false;
+        this.deadlineDate = "";
+        this.journalName = "";
+        this.contactName = "";
+        this.contactEmail = "";
+        this.contactPhone = "";
+        this.KFS = "";
+        //article,reference,additional,original
       }
-      
     }
   }
-}
+};
 </script>
 
 <style>
+
 .formContainer {
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   padding: 20px 6em 20px 6em;
 }
+
 .alert {
   position: fixed;
   z-index: 5;
@@ -210,9 +210,11 @@ export default {
   border: solid black 2px;
   border-radius: 2px;
 }
+
 .alert p {
   margin: 1em 0 1em 0;
 }
+
 .alertButton {
   width: 50%;
 }
@@ -221,11 +223,13 @@ export default {
   margin-top: 2em;
   margin-bottom: 4em;
 }
+
 .branding {
   display: flex;
   width: 100%;
   justify-content: space-between;
 }
+
 .title {
   margin-top: 4em;
   text-align: center;
@@ -233,38 +237,43 @@ export default {
   font-size: 80%;
   font-weight: 300;
 }
+
 .question {
   margin-bottom: 2em;
   font-size: 1.1em;
 }
+
 .question.file {
   margin-top: 3em;
   font-size: 1em;
 }
+
 .oneliner {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
 }
+
 .description {
   width: 100%;
   height: 5em;
   margin-top: 2em;
 }
+
 .deadlineSelect {
   display: flex;
   justify-content: center;
   height: 1em;
-
   align-items: center;
 }
+
 .deadlineSelect * {
   margin-left: 0.7em;
 }
+
 .textInput {
   background: rgba(0, 0, 0, 0.05);
   outline: none;
-  /* border: solid rgba(0,0,0,0.3) 1px; */
   border: none;
   padding: 1em;
   font-size: 0.9em;
@@ -273,13 +282,14 @@ export default {
   border-radius: 3px;
   margin-top: 1em;
 }
+
 .textInput:hover {
   background: rgba(0, 0, 0, 0.1);
 }
+
 .button {
   background: rgba(0, 141, 0, 0.287);
   outline: none;
-  /* border: solid rgba(0,0,0,0.3) 1px; */
   border: none;
   padding: 1em;
   font-size: 0.9em;
@@ -288,12 +298,15 @@ export default {
   border-radius: 3px;
   width: 100%;
 }
+
 .button:hover {
   background: rgba(0, 141, 0, 0.116);
   cursor: pointer;
 }
+
 .button.disabled {
   background: rgba(255, 0, 0, 0.315);
   pointer-events: none;
 }
+
 </style>
